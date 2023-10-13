ad43902e16dc3a6effe33e0f330151d973c5e858
Manuel Teodoro <manuelteodoro@MBP-de-Manuel-3.lan>
2023-10-13 17:36:30 +0100

    started ex01

---
+++ Module_02/ex01/Fixed.hpp
@@ -0,0 +1,22 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+class	Fixed {
+	private:
+		int					rawBits;
+		static const int	fBits = 8;
+	public:
+		Fixed();
+		~Fixed();
+		Fixed(Fixed const &other);
+		Fixed &operator=(Fixed const &other);
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
+
+#endif
