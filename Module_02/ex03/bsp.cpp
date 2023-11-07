b79eeb0a6eca7b585e5df1021595bc89d8e07186
Manuel Tiago Simoes Teodoro <mtiago-s@c2r9s2.42lisboa.com>
2023-11-07 16:21:14 +0000

    checking M02 is ok

--- Module_02/ex03/bsp.cpp
+++ Module_02/ex03/bsp.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bsp.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtiago-s <mtiago-s@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: mtiago-s <mtiago-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 12:04:41 by mtiago-s          #+#    #+#             */
-/*   Updated: 2023/10/24 13:13:54 by mtiago-s         ###   ########.fr       */
+/*   Updated: 2023/11/07 16:20:21 by mtiago-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ bool	bsp(const Point a, const Point b, const Point c, const Point point) {
 	Fixed B = area(point.getX(), point.getY(), b.getX(), b.getY(), c.getX(), c.getY());
 	Fixed C = area(a.getX(), a.getY(), point.getX(), point.getY(), c.getX(), c.getY());
 	Fixed D = area(a.getX(), a.getY(), b.getX(), b.getY(), point.getX(), point.getY());
+	//checks if the point is in one of the lines of the triangle
 	if (B == 0 || C == 0 || D == 0)
 		return (false);
 	//if all areas add up the area A will be the sums of the others. if not the point is outside the triangle
