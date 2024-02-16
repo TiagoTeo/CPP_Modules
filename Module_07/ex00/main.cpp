9462f571490f00e959a8ef356ffdc8458cf10e85
TiagoTeo <manueltiagoteodoro@gmail.com>
2024-02-16 19:15:17 +0000

    started ex02 m09

--- Module_07/ex00/main.cpp
+++ Module_07/ex00/main.cpp
@@ -1,22 +1,21 @@
-#include "Whatever.hpp"
+#include "whatever.hpp"
 
 int main( void ) {
-	Whatever w;
 	int a = 2;
 	int b = 3;
 
-	w.swap( a, b );
+	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << w.min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << w.max( a, b ) << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
 	std::string c = "chaine1";
 	std::string d = "chaine2";
-	w.swap(c, d);
+	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 
-	std::cout << "min( c, d ) = " << w.min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << w.max( c, d ) << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
 	return 0;
 }
